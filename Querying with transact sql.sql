94b9453b1c776a38f20c473aed50b2dee23f5c5e
andrestroy <camusdeacuariogt@gmail.com>
2016-09-15 21:48:24 -0400

    Updated code

--- Querying with transact sql.sql
+++ Querying with transact sql.sql
@@ -16,12 +16,13 @@
 ------------------------- SELECT Basis ----------------------------------
 -- WHERE filter before we grouped it up.
 -- HAVING filters after you aggregated it.
-SELECT ord_add_date, COUNT(ord_number)
-FROM ac_order
-WHERE ord_status = 'Open'
-Group By ord_add_date
-HAVING COUNT(ord_number) > 2
-ORDER BY ord_add_date
+SELECT SellEndDate, COUNT(Size)
+FROM SalesLT.Product
+WHERE Color = 'Red'
+Group By SellEndDate
+Having SellEndDate IS NOT NULL
+ORDER BY SellEndDate
+
 
 ---------------- Order of execution of Select statement
 --5 SELECT <select list>
@@ -31,8 +32,7 @@ ORDER BY ord_add_date
 --4 HAVING <search condition>
 
 -- Concatonate in SQL
-SELECT prd_type + prd_description as TypeDesc
-FROM ac_product 
+SELECT Name +  ProductNumber as ProductDescription FROM SalesLT.Product
 
 -- something + NULL = NULL
 -- cannot add one type to another data type, i.e. int + nvarchar
@@ -43,19 +43,101 @@ FROM ac_product
 -- Explicit Conversion: 
 -- CAST, TRY_CAST, CONVERT, TRY_CONVERT, PARSE, TRY_PARSE, STR
 -- CAST
-select cast(prd_code as varchar(2)) + ': ' + prd_name as ProductName 
-FROM ac_product
+
+SELECT CAST(ProductID as varchar(3)) + ':' + Name as ProductName, ProductID
+FROM SalesLT.Product
 
 -- CONVERT
-select convert(varchar(2), prd_code) + ': ' + prd_name as ProductName
-FROM ac_product
+SELECT CONVERT(varchar(3), ProductID) + ':' + Name as ProductName, ProductID
+FROM SalesLT.Product
 
 -- CONVERT DATES
-SELECT prd_add_date,
-	CONVERT(nvarchar(30), prd_add_date) as ConvertedDate,
-	CONVERT(nvarchar(30), prd_add_date, 126) as ISO8601FormatDate
-FROM ac_product
+
+SELECT SellEndDate, 
+	CONVERT(nvarchar(30), SellEndDate) as ConvertedDate,
+	CONVERT(nvarchar(30), SellStartDate, 126) as ISO8601FormatDate 
+FROM SalesLT.Product
 
 -- TRY to CAST
-SELECT prd_name, TRY_CAST (prd_code AS INTEGER) as NumericSize
-FROM ac_product
+SELECT Name, TRY_CAST(Size as INTEGER) as NumericSize, *
+FROM SalesLT.Product
+
+=========== Working with NULLs ================
+--2 + NULL = NULL
+--'MyString' + NULL = NULL
+-- using compare
+-- IF NULL = NULL returns false
+-- IF NULL IS NULL returns true
+------------- NULL functions -------
+-- ISNULL(column/variable, value)
+-- returns value if the column or variable is NULL, otherwise return the column value.
+
+SELECT ISNULL(SizeIds, 0)
+FROM Product
+
+-- NULLIF(column/variable, value)
+-- Returns NULL if the column or variable is value.
+
+SELECT NULLIF(HasOptions, 0)
+FROM Product
+
+
+-- COALESCE(column/variable1, column/variable2,..)
+-- Returns the value of the first non-NULL column or variable in the list
+
+SELECT COALESCE(SizeIds, Image)
+FROM Product
+
+-- REMOVE DUPLICATES In Transact SQL
+-- Using of DISCTINCT Keyword.
+-- ORDER BY, by default is ASC
+-- TOP keywork
+-- OFFSET keyword
+
+SELECT * FROM ProductSize
+ORDER BY Price DESC
+OFFSET 4 ROWS -- Start selecting by jumping 4 rows
+FETCH NEXT 5 ROWS ONLY -- select only 5 rows from the start index (4)
+
+-- TOP WITH TIES returns also the additional items that honor the order by clause
+-- of the last item returned.
+-- i.e.
+SELECT TOP 8 WITH TIES * FROM dbo.ProductOption
+ORDER BY Type
+
+Id  Type    Name    Factor  IsPizzaOption   IsSaladOption
+13  cheese  Feta    1   1   1
+14  cheese  Fontina & Swiss 1   1   1
+15  cheese  Soy 1   1   1
+16  cheese  Cheddar 1   1   1
+17  cheese  Ricotta 1   1   1
+18  cheese  Gorgonzola  1   1   1
+19  cheese  Mozzarella  1   1   1
+1   crust   Classic Crust   0   1   0
+2   crust   Wheat Crust 0   1   0
+3   crust   Gluten-free Crust   0   1   0
+
+-- this should return only 8 results, but since there are 2 additional items
+-- that has the same type (crust) those are also returned.
+
+-- Filtering and Using Predicates.
+-- =<> compares values for equality
+-- IN Deermines whether a specified value matches any value in a subquery or a list.
+-- BETWEEN Specifies an inclusive range to test.
+
+SELECT SellStartDate FROM SalesLT.Product WHERE SellStartDate BETWEEN '1998/06/01' AND '2001/07/01'
+
+-- LIKE Determines whether a specific character string matches a specified pattern
+-- which can include wildcards.
+
+SELECT * FROM SalesLT.Product WHERE ProductNumber LIKE 'FR-_[0-9]%'
+-- the _ means any literal
+-- the [0-9] specifies that a number between 0 and 9 is required.
+
+
+-- AND Combines two Boolean expressions and returns TRUE only when 
+-- both are TRUE
+-- OR  Combines two Boolean expression and returns OR if either is TRUE
+-- NOT Reverses the result of a search condition.
+
+
